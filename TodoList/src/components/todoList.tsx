f5e1f4c81dcba6756689f6a4f0d270580c386800
YB-BigSwan <swanson.stephen.h@gmail.com>
2023-01-25 20:25:46 +0200

    Todolist app exercise completed

---
+++ TodoList/src/components/todoList.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+interface Todo {
+	description: string;
+	date: string;
+}
+
+export default function TodoList() {
+	const [input, setInput] = useState({
+		description: '',
+		date: '',
+	});
+	const [todo, setTodo] = useState<Array<Todo>>([]);
+
+	const inputChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setInput({ ...input, [event.target.name]: event.target.value });
+	};
+
+	const addTodo = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		if (input.description !== '' && input.date !== '') {
+			setTodo([...todo, input]);
+			setInput({ description: '', date: '' });
+		}
+	};
+
+	return (
+		<div>
+			<h1>Simple To-Do List</h1>
+			{/* description */}
+			<form onSubmit={addTodo}>
+				<input
+					type='text'
+					name='description'
+					placeholder='What do you need to do?'
+					value={input.description}
+					onChange={inputChanged}
+				/>
+				{/* date */}
+				<input
+					type='text'
+					name='date'
+					placeholder='dd.mm.yy'
+					value={input.date}
+					onChange={inputChanged}
+				/>
+				<input type='submit' value='Add to list' />
+			</form>
+			<table>
+				<tbody>
+					{todo.map((todo, index) => (
+						<tr key={index}>
+							<td id='data'>{todo.description}</td>
+							<td id='data'>{todo.date}</td>
+							<td></td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+}
