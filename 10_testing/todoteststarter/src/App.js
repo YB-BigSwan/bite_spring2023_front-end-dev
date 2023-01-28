1ae7da312fe1e95ebdc85a4d6487b9816e86f8aa
YB-BigSwan <swanson.stephen.h@gmail.com>
2023-01-28 09:33:40 +0200

    adds completed clear todos test exercise

--- 10_testing/todoteststarter/src/App.js
+++ 10_testing/todoteststarter/src/App.js
@@ -1,29 +1,47 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoTable from './TodoTable';
 
 function App() {
-  const [todo, setTodo] = useState({desc: '', date: ''});
-  const [todos, setTodos] = useState([]);
+	const [todo, setTodo] = useState({ desc: '', date: '' });
+	const [todos, setTodos] = useState([]);
 
-  const addTodo = (event) => {
-    event.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo({desc: '', date: ''});
-  }
+	const addTodo = (event) => {
+		event.preventDefault();
+		setTodos([...todos, todo]);
+		setTodo({ desc: '', date: '' });
+	};
 
-  const inputChanged = (event) => {
-    setTodo({...todo, [event.target.name]: event.target.value});
-  } 
+	const clearTable = (event) => {
+		event.preventDefault();
+		setTodos([]);
+	};
 
-  return (
-    <div className="App">
-      <input type="text" placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
-      <input type="text" placeholder="Description" name="desc" value={todo.desc} onChange={inputChanged}/>
-      <button onClick={addTodo}>Add</button>
-      <TodoTable todos={todos} />
-    </div>
-  );
+	const inputChanged = (event) => {
+		setTodo({ ...todo, [event.target.name]: event.target.value });
+	};
+
+	return (
+		<div className='App'>
+			<input
+				type='text'
+				placeholder='Date'
+				name='date'
+				value={todo.date}
+				onChange={inputChanged}
+			/>
+			<input
+				type='text'
+				placeholder='Description'
+				name='desc'
+				value={todo.desc}
+				onChange={inputChanged}
+			/>
+			<button onClick={addTodo}>Add</button>
+			<button onClick={clearTable}>Clear Tables</button>
+			<TodoTable todos={todos} />
+		</div>
+	);
 }
 
 export default App;
--- 10_testing/todoteststarter/src/App.test.js
+++ 10_testing/todoteststarter/src/App.test.js
@@ -20,3 +20,18 @@ test('add todo', () => {
 	const tablecell = screen.getByText(/go to coffee/i);
 	expect(tablecell).toBeInTheDocument();
 });
+
+test('add todo and clear', () => {
+	render(<App />);
+	const desc = screen.getByPlaceholderText('Description');
+	fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+	const date = screen.getByPlaceholderText('Date');
+	fireEvent.change(date, { target: { value: '29.01.2021' } });
+	const button = screen.getByText('Add');
+	fireEvent.click(button);
+	const tablecell = screen.getByText(/go to coffee/i);
+	expect(tablecell).toBeInTheDocument();
+	const clearButton = screen.queryByText('Clear Tables');
+	fireEvent.click(clearButton);
+	expect(tablecell).not.toBeInTheDocument();
+});
